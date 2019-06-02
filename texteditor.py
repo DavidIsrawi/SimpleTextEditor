be7d6c1392da1d17077f1c28f5a71bae2fdef5d9
David <davidisrawi@gmail.com>
2019-06-01 20:49:05 -0700

    theme style added

--- texteditor.py
+++ texteditor.py
@@ -33,21 +33,42 @@ def openFile():
     text.insert(0.0, t)
     filename = openedFile.name
 
+def selectDarkTheme():
+    global text
+    text.config(fg="white", bg="black")
+
+def selectWhiteTheme():
+    global text
+    text.config(fg="black", bg="white")
+
 root = Tk()
 root.title("Text Editor")
 
 text = Text(root, width=400, height=400)
 text.pack()
 
+# Menu Bar
 menuBar = Menu(root)
 fileMenu = Menu(menuBar)
+themeMenu = Menu(menuBar)
+
+# Menu: File Load / Save
 fileMenu.add_command(label="New", command=newFile)
 fileMenu.add_command(label="Open", command=openFile)
 fileMenu.add_command(label="Save", command=saveFile)
 fileMenu.add_command(label="Save As...", command=saveFileAs)
 fileMenu.add_separator()
-fileMenu.add_command(label="Quit", command=root.quit)
-menuBar.add_cascade(label="File", menu=fileMenu)
 
+# Menu: Quit
+fileMenu.add_command(label="Quit", command=root.quit)
+
+# Themes
+themeMenu.add_command(label="Dark Theme", command=selectDarkTheme)
+themeMenu.add_command(label="White Theme", command=selectWhiteTheme)
+
+# Add menu to root
+menuBar.add_cascade(label="File", menu=fileMenu)
+menuBar.add_cascade(label="Style", menu=themeMenu)
 root.config(menu=menuBar)
+
 root.mainloop()
